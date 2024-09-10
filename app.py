617dfc51c339a1df2b13bd5f115faa5ecc2ae446
Huenthong_ <126305082+huenthong@users.noreply.github.com>
2024-09-10 18:18:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ if cluster_model == 'KMeans':
     max_iter = st.slider('Maximum iterations', min_value=100, max_value=1000, value=300)
 
 elif cluster_model == 'MeanShift':
-    bandwidth = st.slider('Bandwidth', min_value=0.1, max_value=5.0, value=1.0, step=0.1)
+    bin_seeding = st.checkbox('Use bin seeding', value=False)
+    cluster_all = st.checkbox('Assign all points to a cluster', value=True)
 
 elif cluster_model == 'DBSCAN':
     eps = st.slider('Epsilon', min_value=0.1, max_value=2.0, value=0.5, step=0.1)
@@ -58,7 +59,7 @@ if cluster_model == 'KMeans':
     labels = clustering.fit_predict(df)
 
 elif cluster_model == 'MeanShift':
-    clustering = MeanShift(bandwidth=bandwidth)
+    clustering = MeanShift(bin_seeding=bin_seeding, cluster_all=cluster_all)
     labels = clustering.fit_predict(df)
 
 elif cluster_model == 'DBSCAN':
@@ -87,6 +88,7 @@ if apply_pca:
     st.write('PCA Result:')
     fig, ax = plt.subplots(figsize=(10, 8))
     sns.scatterplot(data=pca_df, x='PC1', y='PC2', hue='Cluster', palette='Set1', ax=ax)
+    ax.legend(loc='best', bbox_to_anchor=(1.05, 1), borderaxespad=0.)
     st.pyplot(fig)
 
 # Silhouette Score
@@ -108,3 +110,4 @@ st.write(cluster_mean_stats)
 st.subheader('Median statistics for each cluster:')
 cluster_median_stats = pd.DataFrame(df).groupby(labels).median()
 st.write(cluster_median_stats)
+
