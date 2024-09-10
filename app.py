2acaf99fc3c022a97c3274e57340757d281c4cdf
Huenthong_ <126305082+huenthong@users.noreply.github.com>
2024-09-10 17:28:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from sklearn.mixture import GaussianMixture
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score
-import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -22,37 +21,35 @@ cluster_model = st.selectbox(
     ('KMeans', 'MeanShift', 'DBSCAN', 'Gaussian Mixture', 'Agglomerative Hierarchical Clustering', 'Spectral Clustering')
 )
 
-# Input for number of clusters (for applicable methods)
+# Additional parameters based on the selected model
 if cluster_model in ['KMeans', 'Gaussian Mixture', 'Agglomerative Hierarchical Clustering', 'Spectral Clustering']:
     n_clusters = st.slider('Number of clusters', min_value=2, max_value=10, value=3)
 
-# Applying PCA for visualization
+if cluster_model == 'DBSCAN':
+    eps = st.slider('Eps (maximum distance between samples)', min_value=0.1, max_value=5.0, value=0.5)
+    min_samples = st.slider('Min samples (number of samples in a neighborhood for a point to be considered as a core point)', min_value=1, max_value=50, value=5)
+    
+if cluster_model == 'Spectral Clustering':
+    n_neighbors = st.slider('Number of neighbors for Spectral Clustering', min_value=2, max_value=30, value=10)
+
+# Apply PCA for visualization
 apply_pca = st.checkbox('Apply PCA for visualization')
 
 # Perform clustering based on selected model
 if cluster_model == 'KMeans':
     clustering = KMeans(n_clusters=n_clusters, n_init=10)
-    labels = clustering.fit_predict(df)
-
 elif cluster_model == 'MeanShift':
     clustering = MeanShift()
-    labels = clustering.fit_predict(df)
-
 elif cluster_model == 'DBSCAN':
-    clustering = DBSCAN()
-    labels = clustering.fit_predict(df)
-
+    clustering = DBSCAN(eps=eps, min_samples=min_samples)
 elif cluster_model == 'Gaussian Mixture':
     clustering = GaussianMixture(n_components=n_clusters)
-    labels = clustering.fit_predict(df)
-
 elif cluster_model == 'Agglomerative Hierarchical Clustering':
     clustering = AgglomerativeClustering(n_clusters=n_clusters)
-    labels = clustering.fit_predict(df)
-
 elif cluster_model == 'Spectral Clustering':
-    clustering = SpectralClustering(n_clusters=n_clusters)
-    labels = clustering.fit_predict(df)
+    clustering = SpectralClustering(n_clusters=n_clusters, n_neighbors=n_neighbors)
+
+labels = clustering.fit_predict(df)
 
 # Display PCA visualization
 if apply_pca:
@@ -86,8 +83,3 @@ st.write('Median statistics for each cluster:')
 cluster_median_stats = pd.DataFrame(df).groupby(labels).median()
 st.write(cluster_median_stats)
 
-
-# Median statistics for each cluster
-st.write('Median statistics for each cluster:')
-cluster_median_stats = pd.DataFrame(df).groupby(labels).median()
-st.write(cluster_median_stats)
