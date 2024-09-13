f1ad518b129c8a9346fb79d3a8e8434bc26b20e0
Huenthong_ <126305082+huenthong@users.noreply.github.com>
2024-09-14 00:44:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from sklearn.mixture import GaussianMixture
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score
-from sklearn.model_selection import GridSearchCV
-import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -27,118 +25,65 @@ cluster_model = st.selectbox(
     ('KMeans', 'MeanShift', 'DBSCAN', 'Gaussian Mixture', 'Agglomerative Hierarchical Clustering', 'Spectral Clustering')
 )
 
-# Input parameters and fine-tuning based on selected clustering model
-if cluster_model == 'KMeans':
+# Input for number of clusters (for applicable methods)
+if cluster_model in ['KMeans', 'Gaussian Mixture', 'Agglomerative Hierarchical Clustering', 'Spectral Clustering']:
     n_clusters = st.slider('Number of clusters', min_value=2, max_value=10, value=3)
+
+# Input parameters based on selected clustering model
+if cluster_model == 'KMeans':
     init_method = st.selectbox('Initialization method', ['k-means++', 'random'])
     max_iter = st.slider('Maximum iterations', min_value=100, max_value=1000, value=300)
-    
-    # Fine-tuning KMeans
-    param_grid = {"n_clusters": [n_clusters]}
-    kmeans_model = GridSearchCV(KMeans(init=init_method, max_iter=max_iter, n_init=10), param_grid, cv=3)
-    kmeans_model.fit(df_scaled)
-    labels = kmeans_model.best_estimator_.predict(df_scaled)
-    st.write(f'Best KMeans Parameters: {kmeans_model.best_params_}')
+    n_init = st.slider('Number of initializations', min_value=1, max_value=20, value=10)
 
 elif cluster_model == 'MeanShift':
-    # Random sampling for MeanShift
-    bandwidth_values = np.linspace(0.8, 1.5, num=10)
-    n_iter = 5
-    best_score = -1
-    best_bandwidth = None
-    for i in range(n_iter):
-        bandwidth = random.choice(bandwidth_values)
-        meanshift = MeanShift(bandwidth=bandwidth)
-        labels = meanshift.fit_predict(df_scaled)
-        if len(np.unique(labels)) > 1:
-            score = silhouette_score(df_scaled, labels)
-            if score > best_score:
-                best_score = score
-                best_bandwidth = bandwidth
-    clustering = MeanShift(bandwidth=best_bandwidth)
-    labels = clustering.fit_predict(df_scaled)
-    st.write(f'Best Bandwidth: {best_bandwidth}')
+    bandwidth = st.slider('Bandwidth', min_value=0.8, max_value=1.5, value=1.18, step=0.1)
 
 elif cluster_model == 'DBSCAN':
-    # Random sampling for DBSCAN
-    eps_values = np.linspace(0.1, 0.5, num=10)
-    min_samples_values = np.arange(3, 12)
-    n_iter = 5
-    best_score = -1
-    best_eps = None
-    best_min_samples = None
-    for i in range(n_iter):
-        eps = random.choice(eps_values)
-        min_samples = random.choice(min_samples_values)
-        dbscan = DBSCAN(eps=eps, min_samples=min_samples)
-        labels = dbscan.fit_predict(df_scaled)
-        if len(np.unique(labels)) > 1:
-            score = silhouette_score(df_scaled, labels)
-            if score > best_score:
-                best_score = score
-                best_eps = eps
-                best_min_samples = min_samples
-    clustering = DBSCAN(eps=best_eps, min_samples=best_min_samples)
-    labels = clustering.fit_predict(df_scaled)
-    st.write(f'Best eps: {best_eps}, Best min_samples: {best_min_samples}')
+    eps = st.slider('Epsilon', min_value=0.1, max_value=0.5, value=0.32, step=0.1)
+    min_samples = st.slider('Minimum samples', min_value=3, max_value=12, value=8)
 
 elif cluster_model == 'Gaussian Mixture':
-    # Fine-tuning Gaussian Mixture
-    param_grid = {"n_components": [n_clusters], "covariance_type": ['full', 'tied', 'diag', 'spherical']}
-    gmm_model = GridSearchCV(GaussianMixture(max_iter=300), param_grid, cv=3)
-    gmm_model.fit(df_scaled)
-    labels = gmm_model.best_estimator_.predict(df_scaled)
-    st.write(f'Best Gaussian Mixture Parameters: {gmm_model.best_params_}')
+    covariance_type = st.selectbox('Covariance type', ['full', 'tied', 'diag', 'spherical'])
+    max_iter = st.slider('Maximum iterations', min_value=100, max_value=1000, value=300)
+    n_init = st.slider('Number of initializations', min_value=1, max_value=20, value=10)
 
 elif cluster_model == 'Agglomerative Hierarchical Clustering':
-    # Random sampling for Agglomerative Clustering
-    linkage_values = ['ward', 'complete', 'average', 'single']
-    n_clusters_values = np.arange(2, 10)
-    n_iter = 5
-    best_score = -1
-    best_n_clusters = None
-    best_linkage = None
-    for i in range(n_iter):
-        n_clusters = random.choice(n_clusters_values)
-        linkage = random.choice(linkage_values)
-        agglomerative = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage, affinity='euclidean')
-        labels = agglomerative.fit_predict(df_scaled)
-        if len(np.unique(labels)) > 1:
-            score = silhouette_score(df_scaled, labels)
-            if score > best_score:
-                best_score = score
-                best_n_clusters = n_clusters
-                best_linkage = linkage
-    clustering = AgglomerativeClustering(n_clusters=best_n_clusters, linkage=best_linkage, affinity='euclidean')
-    labels = clustering.fit_predict(df_scaled)
-    st.write(f'Best n_clusters: {best_n_clusters}, Best linkage: {best_linkage}')
+    affinity = st.selectbox('Affinity', ['euclidean'])
+    linkage = st.selectbox('Linkage', ['ward', 'complete', 'average', 'single'])
 
 elif cluster_model == 'Spectral Clustering':
-    # Random sampling for Spectral Clustering
-    affinity_values = ['nearest_neighbors', 'rbf']
-    n_neighbors_values = np.arange(2, 20)
-    n_iter = 5
-    best_score = -1
-    best_affinity = None
-    best_n_neighbors = None
-    for i in range(n_iter):
-        affinity = random.choice(affinity_values)
-        n_neighbors = random.choice(n_neighbors_values)
-        spectral = SpectralClustering(n_clusters=n_clusters, affinity=affinity, n_neighbors=n_neighbors)
-        labels = spectral.fit_predict(df_scaled)
-        if len(np.unique(labels)) > 1:
-            score = silhouette_score(df_scaled, labels)
-            if score > best_score:
-                best_score = score
-                best_affinity = affinity
-                best_n_neighbors = n_neighbors
-    clustering = SpectralClustering(n_clusters=n_clusters, affinity=best_affinity, n_neighbors=best_n_neighbors)
-    labels = clustering.fit_predict(df_scaled)
-    st.write(f'Best affinity: {best_affinity}, Best n_neighbors: {best_n_neighbors}')
+    affinity = st.selectbox('Affinity', ['nearest_neighbors', 'rbf'])
+    n_neighbors = st.slider('Number of neighbors', min_value=2, max_value=20, value=10)
 
 # Applying PCA for visualization
 apply_pca = st.checkbox('Display PCA Visualization')
 
+# Perform clustering based on selected model
+if cluster_model == 'KMeans':
+    clustering = KMeans(n_clusters=n_clusters, init=init_method, n_init=n_init, max_iter=max_iter)
+    labels = clustering.fit_predict(df_scaled)
+
+elif cluster_model == 'MeanShift':
+    clustering = MeanShift(bandwidth=bandwidth)
+    labels = clustering.fit_predict(df_scaled)
+
+elif cluster_model == 'DBSCAN':
+    clustering = DBSCAN(eps=eps, min_samples=min_samples)
+    labels = clustering.fit_predict(df_scaled)
+
+elif cluster_model == 'Gaussian Mixture':
+    clustering = GaussianMixture(n_components=n_clusters, covariance_type=covariance_type, max_iter=max_iter, n_init=n_init)
+    labels = clustering.predict(df_scaled)
+
+elif cluster_model == 'Agglomerative Hierarchical Clustering':
+    clustering = AgglomerativeClustering(n_clusters=n_clusters, affinity=affinity, linkage=linkage)
+    labels = clustering.fit_predict(df_scaled)
+
+elif cluster_model == 'Spectral Clustering':
+    clustering = SpectralClustering(n_clusters=n_clusters, affinity=affinity, n_neighbors=n_neighbors)
+    labels = clustering.fit_predict(df_scaled)
+
+# Display PCA visualization
 if apply_pca:
     pca = PCA(n_components=2)
     pca_data = pca.fit_transform(df_scaled)
